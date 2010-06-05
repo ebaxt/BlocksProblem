4909e2b81434d5a68d158281969d89988ccbe928
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-06-05 21:37:06 +0800

    Fix whitespace

--- src/main/scala/Blocks/Storage.scala
+++ src/main/scala/Blocks/Storage.scala
@@ -46,20 +46,20 @@ final class Storage(val columns: List[Column]) {
 		}
 	}
 
-  override def equals(o: Any) = o match {
-    case other: Storage => other.columns == columns
-    case _ => false
-  }
+	override def equals(o: Any) = o match {
+		case other: Storage => other.columns == columns
+		case _ => false
+	}
 
-  override def hashCode = columns.hashCode
+	override def hashCode = columns.hashCode
 }
 
 object Storage {
-  def apply(args: Column*): Storage = {
-    new Storage(args.toList)
-  }
+	def apply(args: Column*): Storage = {
+		new Storage(args.toList)
+	}
 
-  def apply(boxes: List[String]): Storage = {
-    new Storage(boxes.map(b => Column(b)))
-  }
+	def apply(boxes: List[String]): Storage = {
+		new Storage(boxes.map(b => Column(b)))
+	}
 }
