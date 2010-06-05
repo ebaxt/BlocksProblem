20238a96ae75c16444cbbb09d855ec6fc093f0bc
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2010-06-06 00:39:08 +0800

    Refactor: remove unintuitive unstack()

--- src/main/scala/Blocks/Storage.scala
+++ src/main/scala/Blocks/Storage.scala
@@ -14,41 +14,39 @@ final class Storage(val columns: List[Column]) {
 	}
 
 	type Action = (Box, Column) => Column
-	type Action2 = (Box, Column) => (Column, Column)
 
 	def applyToMatchingColumns(box: Box, action: Action) =
 		new Storage(columns.map(
 			c => if (c.contains(box)) action(box, c) else c))
 
+	/**
+	 * The top of the column, to and including b.
+	 */
 	private def topTo(b: Box, source: Column, target: Column)
 	: Column = if (source.isEmpty) throw new IllegalStateException()
 	else {
-		if (source.top == b) {
-			target.push(source.top)
-		} else {
-			topTo(b, source.pop, target.push(source.top))
-		}
-	}
-
-	private def pile(box: Box, source: Column): Column = {
-		val unstacked = unstack(box, source, Column())
-		unstacked._1
-	}
-
-	private def move(box: Box, source: Column): Column = {
-		val unstacked = unstack(box, source, Column())
-		(unstacked._1.pushAll(unstacked._2))
+		if (source.top == b) target.push(source.top)
+		else topTo(b, source.pop, target.push(source.top))
 	}
 
 	/**
-	 * Unstacks elements until it encounters box. Returns a tuple with the elements remaining on the stack
-	 * and elements taken from the stack (excluding box)
+	 * Pop elements from the column, to and including b.
 	 */
-	private def unstack(box: Box, source: Column, target: Column): (Column, Column) = if (source.isEmpty) {
-		throw new IllegalStateException()
-	} else {
-		if (box == source.top) (source.pop, target)
-		else unstack(box, source.pop, target.push(source.top))
+	private def popTo(b: Box, source: Column)
+	: Column = if (source.isEmpty) throw new IllegalStateException()
+	else {
+		if (source.top == b) source.pop
+		else popTo(b, source.pop)
+	}
+
+	private def pile(box: Box, source: Column): Column = {
+		popTo(box, source)
+	}
+
+	private def move(box: Box, source: Column): Column = {
+		val remainder = popTo(box, source)
+		val popped = topTo(box, source, Column()).pop
+		remainder.pushAll(popped)
 	}
 
 	override def equals(o: Any) = o match {
